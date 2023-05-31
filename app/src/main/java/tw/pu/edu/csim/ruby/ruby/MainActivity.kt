dea48d8ead364c66dcef18d2d6529f2409229e2b
411017284 <s1101728@gm.pu.edu.tw>
2023-05-31 09:37:58 +0800

    Initial commit

---
+++ app/src/main/java/tw/pu/edu/csim/ruby/ruby/MainActivity.kt
@@ -0,0 +1,43 @@
+package tw.pu.edu.csim.ruby.ruby
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.pu.edu.csim.ruby.ruby.ui.theme.RubyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RubyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RubyTheme {
+        Greeting("Android")
+    }
+}
